9ccf7eaf4485d82d237087947eef3c29ee024bad
ashraful2871 <ashrafulislam2758@gmail.com>
2024-09-22 23:59:38 +0600

    feni and injured  donet part function complete

--- js/script.js
+++ js/script.js
@@ -16,3 +16,36 @@ document
   });
 
 // Feni Donate
+document
+  .getElementById("btn-donate-feni")
+  .addEventListener("click", function () {
+    const feniInputField = getInputValueById("feni-input-field");
+    const feniDonateAmount = getTextValueById("feni-donate-amount");
+    const mainAccountBalanceEl = getTextValueById("main-account-balance");
+
+    const feniTotalDonateAmount = feniDonateAmount + feniInputField;
+    document.getElementById("feni-donate-amount").innerText =
+      feniTotalDonateAmount;
+
+    const mainAccountBalance = mainAccountBalanceEl - feniInputField;
+    document.getElementById("main-account-balance").innerText =
+      mainAccountBalance;
+  });
+
+// Injured Donate
+
+document
+  .getElementById("btn-donate-injured")
+  .addEventListener("click", function () {
+    const injuredInputField = getInputValueById("injured-input-field");
+    const injuredDonateAmount = getTextValueById("injured-donate-amount");
+    const mainAccountBalanceEl = getTextValueById("main-account-balance");
+
+    const injuredTotalDonateAmount = injuredDonateAmount + injuredInputField;
+    document.getElementById("injured-donate-amount").innerText =
+      injuredTotalDonateAmount;
+
+    const mainAccountBalance = mainAccountBalanceEl - injuredInputField;
+    document.getElementById("main-account-balance").innerText =
+      mainAccountBalance;
+  });
